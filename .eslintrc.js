cb8cd8ee605b712f9a6fe85765dc570722b641c1
Rene <rene.souza@gmail.com>
2019-01-07 13:27:07 -0200

    [TDD] - testando com chai. usando webpack para compilar o es6 com o mocha-webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         //"linebreak-style": 0,
         "import/no-extraneous-dependencies":
         ["error", {
-            "devDependencies": true}
-        ]
-    }
+            "devDependencies": true},
+        ],
+        "mocha/no-exclusive-tests": "error"
+    },
+    "plugins": [
+      "mocha"
+    ]
 };
