aca5b41a5a2f7acca01b7470e8cdda899ef75659
Rene Souza <rene.souza@gmail.com>
2019-01-25 21:14:07 -0200

    [Tdd-js] adicionando componente AlbumInfo

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import SpotifyWrapper from 'spotify-wrapper';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQDOTWB5ovaw4mIbwRxJqqtRRPbII3JZaEWD8WNr8Coxl4NAomUVwv_9Y3ca_FBlKOCEptOarAa4lkyn-E4_fKw40pqq3d2xoaIqNjb2WUJ-W4FOBfGV013Gv7LMsRQ_f18bjspvPJMy3fcrpSA',
+  token: 'BQC7d-kwrAszlAF2VjXRsryrYas9tjfvXf77S3Stp8vQto3dKhE-Tri1RWTg1XSuyDpzr2-K65mJGVgXNe8gmXQhllWikI0slNN8ynsw0oGZjvAqPcQXIhqJUuMOipVqw2js6Z61jvVIn_DdrZw',
 });
 
 const albumListElement = document.getElementById('albumList');
+const albumInfoElement = document.getElementById('albumInfo');
 
 const albums = spotify.search.albums('wardruna');
-albums.then((data) => {
-  renderAlbums(data.albums.items, albumListElement);
+const album = spotify.album.getAlbum('0rA5rQVW5kPq0SmUGFtbO1');
+albums
+  .then((data) => {
+    renderAlbums(data.albums.items, albumListElement);
+  });
+
+album.then((data) => {
+  renderAlbumInfo(data, albumInfoElement);
 });
