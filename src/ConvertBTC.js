3063952fb8314fc701457e2cc5e9621598ed458b
Rene Souza <renesouzacv@gmail.com>
2019-02-14 19:42:41 +0000

    [tdd-js] usando Request e Nock para mokar requests em node. Não finalizado ainda

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -4,7 +4,8 @@ function convertBTC(currency = 'USD', amount = 1) {
   const url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=1';
   request(url, (error, response, body) => {
     const apiResponse = JSON.parse(body);
-    console.info(`1 BTC to USD = ${apiResponse.price}`);
+
+    console.info(`${amount} BTC to ${currency} = ${apiResponse.price}`);
   });
 }
 
